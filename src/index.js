722f32d7f3ce8a06cb1c9329c9b3c23f502fdb7c
edmundsamina <edmundsamina@gmail.com>
2023-05-08 11:58:03 +0100

    changing react from react.strictmode

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <React>
   <BrowserRouter>
   <Header />
     <App />
     <Footer/>
     </BrowserRouter>
-  </React.StrictMode>
+  </React>
 );
 
 // If you want to start measuring performance in your app, pass a function
